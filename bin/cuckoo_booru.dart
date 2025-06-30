352b0ed8d7f839e011cc79f95341f24e52b4bd17
Vince Li <vinceli2401@gmail.com>
2025-06-30 08:53:57 -0400

    chore: init

---
+++ bin/cuckoo_booru.dart
@@ -0,0 +1,5 @@
+import 'package:cuckoo_booru/cuckoo_booru.dart' as cuckoo_booru;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cuckoo_booru.calculate()}!');
+}
---
+++ test/cuckoo_booru_test.dart
@@ -0,0 +1,8 @@
+import 'package:cuckoo_booru/cuckoo_booru.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
